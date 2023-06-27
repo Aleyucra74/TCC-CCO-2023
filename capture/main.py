5c3ad518208ba06e74f65e4b79bc1c8acc2963d4
Alexandre Yucra <52496574+Aleyucra74@users.noreply.github.com>
2023-06-27 00:26:54 -0300

    Update main.py

--- capture/main.py
+++ capture/main.py
@@ -8,9 +8,9 @@ import saveImg as sv
 def main():
     today = datetime.now().strftime('%d%m%Y')
 
-    secretKey = "ASIA3JP3R2PLWKOVZIIR"
-    secretAccessKey = "DxMKa/Ju1ZIlc/hM8PbmK3kbjI1C5ORH2fxzbUJv"
-    secretToken = "FwoGZXIvYXdzEPb//////////wEaDEBIgYYtLM3eNm/8aSLHAXc/uwzEu+6WPL7GuYQLWmV/dF5pPcAMoWqYiBaYCF/4FnKmD9rrEeKPaa2tagYKVFNfRwiTrR+0ZDJCUzhupidIInpBZm9Ac7Xsrs/Bo1CxUt51DHjY538T1rDL6pSfJ0y4tVX9safT5WRlXydPjPAVt2mAjm7WMRjUJNP1fB7XM+WmP6cPdAUVdytVOPDpACAWH3FJNCIBBkEkttb+FD8ady27rm0ZFqBYb4cKzm9GTTzTq1d6JRNo85uGu3eJlbCUXJQR6jAomO/npAYyLY85Ao+UryefE/PN5YepTtGxznYIVlVe7M6wKqkn3aysSim7Q28U+QXrPNOTPQ=="
+    secretKey = ""
+    secretAccessKey = ""
+    secretToken = ""
 
     s3Client = connection_aws(secretKey,secretAccessKey,secretToken)
 
