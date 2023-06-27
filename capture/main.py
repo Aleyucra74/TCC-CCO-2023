71841a23db3386eee3ac8ffb11f43df15864174a
aleyucra74 <alexandre.yucra@outlook.com>
2023-06-27 17:26:54 -0300

    Adding the loop for upload predicted files to s3

--- capture/main.py
+++ capture/main.py
@@ -8,9 +8,9 @@ import saveImg as sv
 def main():
     today = datetime.now().strftime('%d%m%Y')
 
-    secretKey = "ASIA3JP3R2PLWKOVZIIR"
-    secretAccessKey = "DxMKa/Ju1ZIlc/hM8PbmK3kbjI1C5ORH2fxzbUJv"
-    secretToken = "FwoGZXIvYXdzEPb//////////wEaDEBIgYYtLM3eNm/8aSLHAXc/uwzEu+6WPL7GuYQLWmV/dF5pPcAMoWqYiBaYCF/4FnKmD9rrEeKPaa2tagYKVFNfRwiTrR+0ZDJCUzhupidIInpBZm9Ac7Xsrs/Bo1CxUt51DHjY538T1rDL6pSfJ0y4tVX9safT5WRlXydPjPAVt2mAjm7WMRjUJNP1fB7XM+WmP6cPdAUVdytVOPDpACAWH3FJNCIBBkEkttb+FD8ady27rm0ZFqBYb4cKzm9GTTzTq1d6JRNo85uGu3eJlbCUXJQR6jAomO/npAYyLY85Ao+UryefE/PN5YepTtGxznYIVlVe7M6wKqkn3aysSim7Q28U+QXrPNOTPQ=="
+    secretKey = "ASIA3JP3R2PLVLBJAZYL"
+    secretAccessKey = "RBpyWIz8n8prZnB0TI0DJ3RPRALlOdQmtN9i0vb3"
+    secretToken = "FwoGZXIvYXdzEAsaDJjZb3XkV4ti/hS91CLHAW3V9Wnf/0UQRrDy/QP9vcID34bQqqkzctc8b7rgPXMRGvQFKQcHDo9onN39FVTlctXTnq82UqbkUt0XXEveiuVoLtxMA9GLg28MGyZ+0LupxU5SNRphWl4SrcHG3Y23e0hY8UzqtnkIchnjgD2eTk1Zdepe8ZV+BQyW6dkIzZ2OiwkTJIAAYNlvS+f8di1L9aO4H0+5PrQg6B/WYzDbLNxNx/uMo5WUxTWU9BgHtL0pgmXLytvyEvy9CzGalZdnMYa+Owsm7MMoy7fspAYyLdaDY6xkLwdSwojCEPT5/eJDkRmMjL3KHC7+devLOtaAIAWnmgTvJcbSTVU78g=="
 
     s3Client = connection_aws(secretKey,secretAccessKey,secretToken)
 
--- classification/main.py
+++ classification/main.py
@@ -1,34 +1,21 @@
 import os
+import re
 import boto3
+import asyncio
 import torch
 import datetime
+import time
 import pandas as pd
 from conn.connectionBoto import connection_aws
 
-def main():        
+async def ultimo_objeto(conn,bucket):
+    #file
+    lastModified = None
 
-    secretKey = "ASIA3JP3R2PLZIUB4O5K"
-    secretAccessKey = "F+dx9qVwTDgEWVEwXF7jSga7Ctu3ls+tv7Gs5P6S"
-    secretToken = "FwoGZXIvYXdzEPr//////////wEaDPzUu3i2fIzM9tk//SLHAcHVGsY9tLjYeGokJlRo5w0Nxh5UBhRwO4fKStEfve0WSeZ2ri07J9DnsJomDgOMELV0pB10uZhsE5zlLH1s0u4NNc21BvzgBmX3WTVNTuXpQ2fkA7JArGnu2lpf/8mb51NHNjcb0Ml8jP8gcK6zgDCtKxrndsSvyFdZwQ8G1IKSOC0W+dEl972itrwzKtgYxK/pjp37ukPCZQUV24XLWt6JYgxEiXcDFycV15VZ1Ke+HIms58AWjZnrHKsAXKKnaJvJ6OY1w7YoztTopAYyLS2WSCO3VsDuPsfGwyVo+RkoAlTPJ+66ofu36ZaaEfx4tAACZnKi57rODXfoyA=="
-    bucketName = "s3-data-tcc-raw"
-
-    s3Client = connection_aws(secretKey,secretAccessKey,secretToken)
-
-    #load models
-    modelWeapon = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/bestgun.pt', trust_repo=True)
-    modelHelmet = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/besthelmet.pt',trust_repo=True)
-    modelHoodie = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/besthoodie.pt',trust_repo=True)
-
-    modelPeople = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/bestperson.pt',trust_repo=True)
-    modelPeople.classes = 0
-
-    models = [modelHelmet, modelHoodie, modelPeople, modelWeapon]
-
-    paginator = s3Client.get_paginator('list_objects')
-    page_iterator = paginator.paginate(Bucket=bucketName)
+    paginator = conn.get_paginator('list_objects')
+    page_iterator = paginator.paginate(Bucket=bucket)
 
     #get the last file uploaded in s3bucket
-    lastModified = None
     for page in page_iterator:
         if "Contents" in page:
             latest2 = max(page['Contents'], key=lambda x: x['LastModified'])
@@ -37,27 +24,73 @@ def main():
             path, filename = os.path.split(lastModified['Key'])
             if not os.path.exists(path):
                 os.makedirs(path)
-            s3Client.download_file(bucketName, lastModified['Key'], lastModified['Key'])
+            conn.download_file(bucket, lastModified['Key'], lastModified['Key'])
             print(f"Downloaded:{lastModified['Key']}")
     
-    predicts = pd.DataFrame()
-    #use models
-    for model in models:
-        predict = model(lastModified['Key'])
-        df = predict.pandas().xyxy[0]
-        df['filename'] = lastModified['Key']
-        predicts = predicts.append(df, ignore_index=True)
-        print(predicts)
+    path, filename = os.path.split(lastModified['Key'])
 
-    predicts.to_csv(lastModified['Key'].split('/')[1]+".csv", sep=';',index=False)
-    #save generated csv to s3 bucket
-    # s3-data-tcc-processed
-    #not working
-    s3Client.upload_file(
-                    fr'C:\Users\alexa\Documents\BANDTEC\TCC\TCC-CCO-2023\classification\{lastModified['Key'].split('/')[1]}.csv',
-                     "s3-data-tcc-processed",
-                    f"{today}/{filename}.jpg"
-                )
+    pattern = r"\d{8}"
+    match = re.search(pattern, filename)
+    
+    if match:
+        date = match.group()
+        print(date)
+    else:
+        print("nenhuma data encontrada")
+
+    if lastModified != None:
+        return lastModified
+    return lastModified
+
+#load models
+modelWeapon = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/bestgun.pt', trust_repo=True)
+modelHelmet = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/besthelmet.pt',trust_repo=True)
+modelHoodie = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/besthoodie.pt',trust_repo=True)
+modelPeople = torch.hub.load('ultralytics/yolov5', 'custom', path='./MODELOS/bestperson.pt',trust_repo=True)
+modelPeople.classes = 0
+os.system('cls')
+ultimoPredict = None
+
+async def main():        
+    global ultimoPredict
+
+    secretKey = "ASIA3JP3R2PLVLBJAZYL"
+    secretAccessKey = "RBpyWIz8n8prZnB0TI0DJ3RPRALlOdQmtN9i0vb3"
+    secretToken = "FwoGZXIvYXdzEAsaDJjZb3XkV4ti/hS91CLHAW3V9Wnf/0UQRrDy/QP9vcID34bQqqkzctc8b7rgPXMRGvQFKQcHDo9onN39FVTlctXTnq82UqbkUt0XXEveiuVoLtxMA9GLg28MGyZ+0LupxU5SNRphWl4SrcHG3Y23e0hY8UzqtnkIchnjgD2eTk1Zdepe8ZV+BQyW6dkIzZ2OiwkTJIAAYNlvS+f8di1L9aO4H0+5PrQg6B/WYzDbLNxNx/uMo5WUxTWU9BgHtL0pgmXLytvyEvy9CzGalZdnMYa+Owsm7MMoy7fspAYyLdaDY6xkLwdSwojCEPT5/eJDkRmMjL3KHC7+devLOtaAIAWnmgTvJcbSTVU78g=="
+    bucketName = "s3-data-tcc-raw"
+
+    s3Client = connection_aws(secretKey,secretAccessKey,secretToken)
+    models = [modelHelmet, modelHoodie, modelPeople, modelWeapon]
+
+    ultimoModificado = await ultimo_objeto(s3Client, bucketName)
+
+    if ultimoPredict != ultimoModificado['Key']:
+        filename = ultimoModificado['Key'].split('/')[-1]
+        predicts = pd.DataFrame()
+        #use models
+        for model in models:
+            predict = model(ultimoModificado['Key'])
+            df = predict.pandas().xyxy[0]
+            df['filename'] = filename
+            predicts = pd.concat([predicts,df], ignore_index=True)
+        folder = ultimoModificado['Key'].split('/')[0]
+        output_filename = filename.split('.')[0]+".csv"
+        predicts.to_csv(folder+'\/'+output_filename, sep=';',index=False)
+        print(predicts)
+        ultimoPredict = ultimoModificado['Key']
+        #save generated csv to s3 bucket
+        s3Client.upload_file(
+                        fr'C:\Users\alexa\Documents\BANDTEC\TCC\TCC-CCO-2023\classification\{folder}\{output_filename}',
+                        "s3-data-tcc-processed",
+                        f"{folder}/{output_filename}"
+                    )
+        print(fr'C:\Users\alexa\Documents\BANDTEC\TCC\TCC-CCO-2023\classification\{folder}\{output_filename}')
+        print(f"{folder}/{output_filename}")
+        print("File Uploaded to S3...")
+        time.sleep(10)
+    else:
+        print("Nenhum arquivo adicionado recentemente...")
 
 if __name__ == "__main__":
-    main()
+    while True:
+        asyncio.run(main())
