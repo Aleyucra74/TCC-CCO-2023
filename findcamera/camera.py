0949568ef1d0c1c8ecf8745649ec810d8c873ced
Aleyucra74 <alexandre.yucra@outlook.com>
2023-08-15 23:18:14 -0300

    algorithm to find the ip camera

---
+++ findcamera/camera.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+
+#cap = cv2.VideoCapture('http://192.168.15.235')
+cap = cv2.VideoCapture('rtsp://admin:#Gf37624352869@192.168.15.235/live/ch00_0')
+
+while(True):
+    ret, frame = cap.read()
+    cv2.imshow("Frame",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
